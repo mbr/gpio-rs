cd3f842d8aa3f7649a084be2e7c0fa681f830db2
Marc Brinkmann <git@marcbrinkmann.de>
2018-03-04 19:25:04 +0100

    Added example.

--- src/lib.rs
+++ src/lib.rs
@@ -12,9 +12,35 @@
 //! [Linux GPIO Sysfs](https://www.kernel.org/doc/Documentation/gpio/sysfs.txt)
 //! interface, found inside the `sysfs` crate.
 //!
-//! ### TODO
+//! ## Example
+//!
+//! ```rust,no_run
+//! use gpio::{GpioIn, GpioOut};
+//! use std::{thread, time};
+//!
+//! // Let's open GPIO23 and -24, e.g. on a Raspberry Pi 2.
+//! let mut gpio23 = gpio::sysfs::SysFsGpioInput::open(23).unwrap();
+//! let mut gpio24 = gpio::sysfs::SysFsGpioOutput::open(24).unwrap();
+//!
+//! // GPIO24 will be toggled every second in the background by a different thread
+//! let mut value = false;
+//! thread::spawn(move || loop {
+//!     gpio24.set_value(value).expect("could not set gpio24");
+//!     thread::sleep(time::Duration::from_millis(1000));
+//!     value = !value;
+//! });
+//!
+//! // The main thread will simply display the current value of GPIO23 every 100ms.
+//! loop {
+//!     println!("GPIO23: {:?}", gpio23.read_value().unwrap());
+//!     thread::sleep(time::Duration::from_millis(100));
+//! }
+//! ```
+//!
+//! ## TODO
 //!
 //! * `/dev/mem` interface: Higher frequency port usage
+//!
 
 pub mod sysfs;
 
