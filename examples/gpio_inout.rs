4c6721620b68cb448721acebaa54f7bb2a6fe76c
Andreas Fackler <AndreasFackler@gmx.de>
2018-04-03 17:48:17 +0200

    Add an edge iterator.

--- examples/gpio_inout.rs
+++ examples/gpio_inout.rs
@@ -1,6 +1,6 @@
 //! Raspberry Pi 2 GPIO example
 //!
-//! Reads from one GPIO, while togging output on the other
+//! Reads from one GPIO, while toggling output on the other
 
 extern crate gpio;
 
@@ -9,7 +9,7 @@ use gpio::{GpioIn, GpioOut};
 use std::{thread, time};
 
 fn main() {
-    let mut gpio23 = gpio::sysfs::SysFsGpioInput::open(23).expect("could not open GPIO23");
+    let gpio23 = gpio::sysfs::SysFsGpioInput::open(23).expect("could not open GPIO23");
     let mut gpio24 = gpio::sysfs::SysFsGpioOutput::open(24).expect("could not open GPIO24");
 
     // start a thread to toggle the gpio on and off at a different rate
